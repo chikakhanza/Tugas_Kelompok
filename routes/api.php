6734ad39ca0a213ca1f5667de59881180dab4d47
chikakhanza <chikafebria26@gmail.com>
2025-07-03 10:59:37 +0700

    chika_memperbaikiapi

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\HomestayController;
-use App\Http\Controllers\Api\UserControllerController;
+use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\BookingController;
 use App\Http\Controllers\Api\PaymentController;
 
 Route::apiResource('homestays', HomestayController::class);
-Route::apiResource('users', UserControllerController::class);
-Route::apiResource('bookings', BookingControllerController::class);
+Route::apiResource('users', UserController::class);
+Route::apiResource('bookings', BookingController::class);
 Route::apiResource('payments', PaymentController::class);
